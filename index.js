7d35385a6e4333f1bc1537d9b6acf4058acca5e8
saidamst2003 <saida2003mst@gmail.com>
2025-02-07 19:30:00 +0100

    j'ai travaillé challenge de'changer la couleur de fond d’une page '

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+//1
 const bouton = document.getElementById('monBouton');
 const paragraphe = document.getElementById('monParagraphe');
 
@@ -7,6 +9,7 @@ bouton.addEventListener('click', function() {
     paragraphe.textContent = "Le texte a été changé après le clic !";
 });
 
+//2
 let compteur = 0;
   const bouton1 = document.getElementById("compteur");
   const affichage = document.getElementById("affichage");
@@ -17,6 +20,9 @@ let compteur = 0;
       affichage.innerHTML = compteur;
   });
 
+
+
+//3
 // Récupération des éléments HTML nécessaires
   const inputTexte = document.getElementById("texteInput");
   const boutonMajuscule = document.getElementById("afficherMajuscule");
@@ -28,3 +34,25 @@ let compteur = 0;
     const texteMajuscule = inputTexte.value.toUpperCase();
     affichageResultat.textContent = texteMajuscule;
   });
+
+//4
+
+      // Récupération des boutons par leur ID
+      const boutonRouge = document.getElementById("rouge");
+      const boutonBleu = document.getElementById("bleu");
+      const boutonVert = document.getElementById("vert");
+  
+      // Écouteur d'événement pour changer la couleur de fond en rouge
+      boutonRouge.addEventListener("click", function () {
+        document.body.style.backgroundColor = "red";
+      });
+  
+      // Écouteur d'événement pour changer la couleur de fond en bleu
+      boutonBleu.addEventListener("click", function () {
+        document.body.style.backgroundColor = "blue"; 
+      });
+  
+      // Écouteur d'événement pour changer la couleur de fond en vert
+      boutonVert.addEventListener("click", function () {
+        document.body.style.backgroundColor = "green"; 
+      });
