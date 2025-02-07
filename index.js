0210bfe67095a2adbfb76236af1bb66491b14e95
saidamst2003 <saida2003mst@gmail.com>
2025-02-07 18:51:52 +0100

    j'ai travaillé challenge de' Compteur de clics '

--- index.js
+++ index.js
@@ -6,3 +6,13 @@ bouton.addEventListener('click', function() {
     // Changer le texte du paragraphe
     paragraphe.textContent = "Le texte a été changé après le clic !";
 });
+
+let compteur = 0;
+  const bouton1 = document.getElementById("compteur");
+  const affichage = document.getElementById("affichage");
+  
+  bouton1.addEventListener("click", function () {
+      // Incrémenter le compteur
+      compteur++; 
+      affichage.innerHTML = compteur;
+  });
